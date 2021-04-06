39c64480728b2bc8a5098eccb4e181f2eb6918d3
Tyler Lewis <tyler.lewis.developer@gmail.com>
2021-04-06 14:35:28 -0700

    setup Campground model

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+
+mongoose.connection.on("error", console.error.bind(console, "connection error: "));
+mongoose.connection.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
 
 
 // setup for ejs
@@ -14,6 +30,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) =>{
     res.render('home');
+});
+
+
+
+app.get('/makecampground', async (req, res) =>{
+    const camp = new Campground({
+        title: 'My Backyard',
+        description: 'Cheap camping',
+    });
+    await camp.save();
+    res.send(camp);
 });
 
 
